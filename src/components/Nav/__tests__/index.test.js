872f72424b280d90d7bd674dd5387766d2f0ba4b
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-04-26 18:37:04 -0700

    tests for about and nav pass

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    // baseline test
+    it("renders", () => {
+        render(<Nav />);
+    })
+
+    // snapshot test
+    it("matches snapshot", () => {
+        const { asFragment } = render(<Nav />);
+
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
+
+
+describe('emoji is visble', () => {
+    it('inserts emoji into the h2', () => {
+        // arrange
+        const { getByLabelText } = render(<Nav />);
+        // assert
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    })
+})
