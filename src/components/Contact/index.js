7731e068d91ae18796fbad847d03ef095da3b28c
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-04-28 16:57:24 -0700

    form and form handlers created

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,37 @@
 import React, { useState } from 'react'
 
-const [formState, setFormState] = useState({ name:'', email: '', message:'' });
+
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
+
         <section>
             <h1>Contact Me</h1>
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
